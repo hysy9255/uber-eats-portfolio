4fb491991f4cedd381dd2efa8b803263e2b35f50
HyungJoonYoon <hysy9255@gmail.com>
2026-05-18 16:06:02 +0900

    env change

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,8 +45,8 @@ import { DataSource } from 'typeorm';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      // envFilePath: '.env.development.local',
-      envFilePath: '.env',
+      envFilePath: '.env.development.local',
+      // envFilePath: '.env',
     }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
@@ -56,9 +56,9 @@ import { DataSource } from 'typeorm';
         username: process.env.DATABASE_USERNAME,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        // ssl: {
+        //   rejectUnauthorized: false,
+        // },
         entities: [
           VehicleEntity,
           DriverDocsEntity,
